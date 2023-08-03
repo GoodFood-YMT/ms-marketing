b9f8d1e69353d0bae73fdc104a1a51d30697e67a
Yasin2719 <karakusyasin19@gmail.com>
2023-08-03 23:16:46 +0200

    feat(): listening RabbitMq queues & store in db

--- start/rabbit.ts
+++ start/rabbit.ts
@@ -1,33 +1,105 @@
-const QUEUE_NAME = 'marketing.order.created'
+const OREDR_CREATED_QUEUE = 'marketing.order.created'
+const DELIVERY_DELIVERED_QUEUE = 'marketing.delivery.delivered'
+const USER_CREATED_QUEUE = 'marketing.user.created'
 
 import Rabbit from '@ioc:Adonis/Addons/Rabbit'
 import { validator, schema } from '@ioc:Adonis/Core/Validator'
+import { prisma } from '@ioc:Adonis/Addons/Prisma'
 
 const OrderCreationSchema = schema.create({
-  orderId: schema.string(),
+  order_id: schema.string(),
   totalPrice: schema.string(),
-  createdAt: schema.string(),
+  created_at: schema.string(),
+  restaurant_id: schema.string(),
+  user_id: schema.string(),
 })
 
-async function listen() {
+const DeliveryCreationSchema = schema.create({
+  delivery_id: schema.string(),
+  order_id: schema.string(),
+  created_at: schema.string(),
+  updated_at: schema.string(),
+  restaurant_id: schema.string(),
+})
+
+const UserCreationSchema = schema.create({
+  user_id: schema.string(),
+  created_at: schema.string(),
+})
+
+async function listenOrderCreated() {
   console.log('marketing.order.created queue started')
-
-  await Rabbit.assertQueue(QUEUE_NAME)
-
-  await Rabbit.consumeFrom(QUEUE_NAME, async (message) => {
+  await Rabbit.assertQueue(OREDR_CREATED_QUEUE)
+  await Rabbit.consumeFrom(OREDR_CREATED_QUEUE, async (message) => {
     try {
-      console.log(JSON.parse(message.content))
       const payload = await validator.validate({
         schema: OrderCreationSchema,
         data: JSON.parse(message.content),
       })
-      console.log(payload)
+      const order = await prisma.orders.create({
+        data: {
+          id: payload.order_id,
+          created_at: new Date(payload.created_at),
+          totalPrice: parseFloat(payload.totalPrice),
+          restaurant_id: payload.restaurant_id,
+          user_id: payload.user_id,
+        },
+      })
+      console.log(`a new order with id=${order.id} has been created`)
     } catch (e) {
-      console.log('SendMail payload not valid', e.message)
+      console.log('marketing.order.created payload not valid', e.message)
     }
-
     message.ack()
   })
 }
 
-listen()
+async function listenDeliveryDelivered() {
+  console.log('marketing.delivery.delivered queue started')
+  await Rabbit.assertQueue(DELIVERY_DELIVERED_QUEUE)
+  await Rabbit.consumeFrom(DELIVERY_DELIVERED_QUEUE, async (message) => {
+    try {
+      const payload = await validator.validate({
+        schema: DeliveryCreationSchema,
+        data: JSON.parse(message.content),
+      })
+      const order = await prisma.deliveries.create({
+        data: {
+          id: payload.delivery_id,
+          order_id: payload.order_id,
+          created_at: new Date(payload.created_at),
+          restaurant_id: '',
+        },
+      })
+      console.log(`a delivery with id=${order.id} has been delivered`)
+    } catch (e) {
+      console.log('marketing.delivery.delivered payload not valid', e.message)
+    }
+    message.ack()
+  })
+}
+async function listenUserCreated() {
+  console.log('marketing.user.created queue started')
+  await Rabbit.assertQueue(USER_CREATED_QUEUE)
+  await Rabbit.consumeFrom(USER_CREATED_QUEUE, async (message) => {
+    try {
+      const payload = await validator.validate({
+        schema: UserCreationSchema,
+        data: JSON.parse(message.content),
+      })
+      const order = await prisma.users.create({
+        data: {
+          id: payload.user_id,
+          created_at: new Date(payload.created_at),
+        },
+      })
+      console.log(`a new user with id=${order.id} has been created`)
+    } catch (e) {
+      console.log('marketing.user.created payload not valid', e.message)
+    }
+    message.ack()
+  })
+}
+
+listenOrderCreated()
+listenDeliveryDelivered()
+listenUserCreated()
