0832d5496e6f867e18acbb6b4e2c1e1fad641c8d
Yasin2719 <karakusyasin19@gmail.com>
2023-08-04 09:46:15 +0200

    fix(): fixed some errors

--- app/Controllers/Http/OrdersController.ts
+++ app/Controllers/Http/OrdersController.ts
@@ -1,21 +1,7 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
-import { prisma } from '@ioc:Adonis/Addons/Prisma'
 
 export default class OrdersController {
   public async index({ response }: HttpContextContract) {
-    const orders = await prisma.order.findMany()
-    return response.status(200).json(orders)
-  }
-
-  public async store({ response }: HttpContextContract) {
-    const order = await prisma.order.create({
-      data: {
-        id: '1',
-        status: 'En Cours',
-        created_at: new Date(),
-        updated_at: null,
-      },
-    })
-    return response.status(200).json(order)
+    return response.status(200).json('hello')
   }
 }
--- start/routes.ts
+++ start/routes.ts
@@ -1,7 +1,4 @@
 import Route from '@ioc:Adonis/Core/Route'
 
 Route.get('/marketing/order', 'OrdersController.index')
-Route.post('/marketing/order', 'OrdersController.store')
-
 Route.get('/marketing/delivery', 'DeliveriesController.index')
-Route.post('/marketing/delivery', 'DeliveriesController.store')
