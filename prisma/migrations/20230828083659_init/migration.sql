92aed1144733990f638aa4a69a94c749ec0b5660
Yasin2719 <karakusyasin19@gmail.com>
2023-08-28 10:45:18 +0200

    feat(): used postgresql

---
+++ prisma/migrations/20230828083659_init/migration.sql
@@ -0,0 +1,30 @@
+-- CreateTable
+CREATE TABLE "Orders" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL,
+    "totalPrice" DOUBLE PRECISION NOT NULL,
+    "restaurantId" TEXT NOT NULL,
+    "userId" TEXT NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "Deliveries" (
+    "id" TEXT NOT NULL,
+    "orderId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "Users" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Orders_id_key" ON "Orders"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Deliveries_id_key" ON "Deliveries"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Users_id_key" ON "Users"("id");
